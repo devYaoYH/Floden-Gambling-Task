2451031c95afe7ab08406b8b62c9e95605046840
devYaoYH <yaoyiheng@gmail.com>
2024-02-18 21:47:28 +0800

    Update card back color and WIN text. Update UI references to score with 1079.

--- StateMachine.js
+++ StateMachine.js
@@ -115,7 +115,7 @@ const TRIAL_BLOCK_SEQUENCE = [
   // Summary page.
   {
     instruction_text: `
-      THANK YOU FOR PLAYING. YOU WON {X} DOLLARS.
+      THANK YOU FOR PLAYING. YOU WON $$ {X} DOLLARS.
       PLEASE HIT END TO COMPLETE THE STUDY.
     `,
     expt_args: null,
